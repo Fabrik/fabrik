e1e43ffa8ba791cd92088ceed641de1dc137c140
s6930064 <s6930064@hotmail.com>
2012-01-22 13:04:45 +0200

    Update components/com_fabrik/models/list.php

--- components/com_fabrik/models/list.php
+++ components/com_fabrik/models/list.php
@@ -6886,7 +6886,8 @@ class FabrikFEModelList extends JModelForm {
 	public function setRenderContext($id)
 	{
 		// $$$ rob if admin filter task = ilter and not list.filter
-		if (JRequest::getVar('task') == 'list.filter' || JRequest::getVar('task') == 'filter') {
+		//if (JRequest::getVar('task') == 'list.filter' || JRequest::getVar('task') == 'filter') {
+		if ((JRequest::getVar('task') == 'list.filter' || JRequest::getVar('task') == 'filter') and (JRequest::getVar('listref') ! = '')) {
 			$listref = JRequest::getVar('listref');
 			$listref = explode('_', $listref);
 			array_shift($listref);
