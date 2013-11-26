a91ee279a14517aa365d8a5656e0fc1225755454
Jean-François Questiaux <info@betterliving.be>
2013-11-26 09:01:15 +0100

    Update list.php
    Enables translation for list empty message

--- components/com_fabrik/models/list.php
+++ components/com_fabrik/models/list.php
@@ -5607,7 +5607,7 @@ class FabrikFEModelList extends JModelForm
 
 		$params = $this->getParams();
 
-		return $params->get('empty_data_msg', JText::_('COM_FABRIK_LIST_NO_DATA_MSG'));
+		return JText::_($params->get('empty_data_msg', JText::_('COM_FABRIK_LIST_NO_DATA_MSG')));
 	}
 
 	/**
